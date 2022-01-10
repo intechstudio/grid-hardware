11d0b30c236398eed0c385f9233064e7476eb943
suku <sukuwc@riseup.net>
2022-01-10 17:18:10 +0100

    SUKU xy workflow updated

---
+++ .github/workflows/convert_bottom_position.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+
+if len(sys.argv) != 3:
+  print 'Invalid Number of arguments:', len(sys.argv), 'arguments.'
+  print 'Argument List:', str(sys.argv)
+  quit()
+
+input_file = open(sys.argv[1])
+output_file = open(sys.argv[2], 'w')
+
+startwriter = csv.writer(output_file)
+
+csv_f = csv.reader(input_file)
+
+
+for row in csv_f:
+  print row
+  if row[6] == 'bottom':
+    row[5] = float(row[5])+180.0
+    if row[5] >= 360.0:
+      row[5] -= 360.0
+  startwriter.writerow(row)
+  print row
+
