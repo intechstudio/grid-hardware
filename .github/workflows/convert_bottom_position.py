937454ccbcba54ae2ea53b43d1848894b5633ac8
suku <sukuwc@riseup.net>
2022-01-10 17:30:14 +0100

    SUKU print syntax fixed

--- .github/workflows/convert_bottom_position.py
+++ .github/workflows/convert_bottom_position.py
@@ -2,7 +2,7 @@ import csv
 import sys
 
 if len(sys.argv) != 3:
-  print 'Invalid Number of arguments'
+  print('Invalid Number of arguments')
   quit()
 
 input_file = open(sys.argv[1])
@@ -14,11 +14,11 @@ csv_f = csv.reader(input_file)
 
 
 for row in csv_f:
-  print row
+  print(row)
   if row[6] == 'bottom':
     row[5] = float(row[5])+180.0
     if row[5] >= 360.0:
       row[5] -= 360.0
   startwriter.writerow(row)
-  print row
+  print(row)
 
