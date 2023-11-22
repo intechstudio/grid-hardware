687d5e6e4f570427695ce4d4a1483164790bea22
suku <sukuwc@riseup.net>
2023-11-22 09:00:00 +0100

    SUKU suku frontpanel files and cicd full cleanup

--- Mechanical/Design/Frontpanel/export_action.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import FreeCAD
-App.openDocument("Mechanical/Design/Frontpanel/grid_frontpanel_new.FCStd")
-objs = App.ActiveDocument.RootObjects
-for obj in objs:
-	sono=App.ActiveDocument.getObject(obj.Name)
-	print(obj.Label)
-	sono.Shape.exportStep("temp/"+obj.Label+".step")
